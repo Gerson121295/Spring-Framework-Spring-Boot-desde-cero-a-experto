87e5538fa09d983b2e72b29f67197f2649277419
Gerson121295 <Gerson21millones@gmail.com>
2024-02-11 11:26:25 -0600

    08 New Spring Boot 3 - Programacion orientada a aspectos AOP - Optimizacion 1.

--- new-spring-boot-webapp/springboot-aop/springboot-aop/src/main/java/com/gerson/springboot/app/aop/springbootaop/aop/GreetingAspect.java
+++ new-spring-boot-webapp/springboot-aop/springboot-aop/src/main/java/com/gerson/springboot/app/aop/springbootaop/aop/GreetingAspect.java
@@ -10,6 +10,7 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
@@ -23,12 +24,17 @@ import org.springframework.stereotype.Component;
 public class GreetingAspect {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    //Metodo para definir el punto de corte para evitar repetir en cada metodo
+    @Pointcut("execution(* com.gerson.springboot.app.aop.springbootaop.services.GreetingService.*(..))") //punto de corte
+    private void greetingLoggerPointCut(){}
+
     //Before especifica que el metodo loggerBefore se ejecutara antes que el metodo especificado en execution
     //@Before("execution(* *.*(..))") //se aplica a todo //muy generico no se recomienda
     //@Before("execution(* com.gerson.springboot.app.aop.springbootaop..*.*(..))") 
     //@Before("execution(* com.gerson.springboot.app.aop.springbootaop.services.*.*(..))") // Agregar * en el tipo de dato a retornar "seria cualquier". //Punto de corte cual metodo o package vamos a interceptar. Execution recive("String: tipo de dato que devuelve el metodo, la ruta completa, nombre de la clase y el metodo y sus argumentos") al agregar el GreetingService.*(..) todas las clases que implementen esta interfaz
     //@Before("execution(String com.gerson.springboot.app.aop.springbootaop.services.GreetingService.sayHello(..))") //Punto de corte cual metodo o package vamos a interceptar. Execution recive("String: tipo de dato que devuelve el metodo, la ruta completa, nombre de la clase y el metodo y sus argumentos") todas las clases que implementen esta interfaz
-    @Before("execution(* com.gerson.springboot.app.aop.springbootaop.services.GreetingService.*(..))") 
+    //@Before("execution(* com.gerson.springboot.app.aop.springbootaop.services.GreetingService.*(..))") 
+    @Before("greetingLoggerPointCut()") //Se envia el nombre del metodo que contiene el punto de corte
     public void loggerBefore(JoinPoint joinPoint){ //Este metodo se va a a ejecutar antes del metodo especificado en @Before
         String method = joinPoint.getSignature().getName(); //Obtenemos el nombre del metodo
         String args = Arrays .toString(joinPoint.getArgs());//Obtenemos los argumentos del metodo
@@ -37,7 +43,8 @@ public class GreetingAspect {
     }
 
     //After - Especifica que el metodo loggerAfter se ejecutara despues que el metodo especificado en execution
-    @After("execution(* com.gerson.springboot.app.aop.springbootaop.services.GreetingService.*(..))") 
+    //@After("execution(* com.gerson.springboot.app.aop.springbootaop.services.GreetingService.*(..))") 
+    @After("greetingLoggerPointCut()")
     public void loggerAfter(JoinPoint joinPoint){ //Este metodo se va a a ejecutar antes del metodo especificado en @After
         String method = joinPoint.getSignature().getName(); //Obtenemos el nombre del metodo
         String args = Arrays .toString(joinPoint.getArgs());//Obtenemos los argumentos del metodo
@@ -47,16 +54,18 @@ public class GreetingAspect {
 
 
         //AfterReturning - Especifica que el metodo loggerAfterReturning se ejecutara despues que el metodo especificado en execution retorne
-        @AfterReturning("execution(* com.gerson.springboot.app.aop.springbootaop.services.GreetingService.*(..))") 
+        //@AfterReturning("execution(* com.gerson.springboot.app.aop.springbootaop.services.GreetingService.*(..))") 
+        @AfterReturning("greetingLoggerPointCut()")
         public void loggerAfterReturning(JoinPoint joinPoint){ //Este metodo se va a a ejecutar antes del metodo especificado en @Before
             String method = joinPoint.getSignature().getName(); //Obtenemos el nombre del metodo
             String args = Arrays .toString(joinPoint.getArgs());//Obtenemos los argumentos del metodo
     
             logger.info("Despues de retornar:" + method + " con los argumentos " + args);
-        }
+    }
 
     //AfterThrowing - Especifica que el metodo loggerAfterThrowing se ejecutara despues que el metodo especificado en execution tenga un error o lanze una excepcion
-    @AfterThrowing("execution(* com.gerson.springboot.app.aop.springbootaop.services.GreetingService.*(..))") 
+    //@AfterThrowing("execution(* com.gerson.springboot.app.aop.springbootaop.services.GreetingService.*(..))") 
+    @AfterThrowing("greetingLoggerPointCut()")
     public void loggerAfterTrowing(JoinPoint joinPoint){ //Este metodo se va a a ejecutar antes del metodo especificado en @Before
         String method = joinPoint.getSignature().getName(); //Obtenemos el nombre del metodo
         String args = Arrays .toString(joinPoint.getArgs());//Obtenemos los argumentos del metodo
@@ -66,7 +75,8 @@ public class GreetingAspect {
 
 
     //Con @Around envuelve todo, podemos implementar la funcionalidad del @Before, @After y el @AfterThrowing(cuando se lanza una excepcion o surge un error)
-    @Around("execution(* com.gerson.springboot.app.aop.springbootaop.services.GreetingService.*(..))") 
+    //@Around("execution(* com.gerson.springboot.app.aop.springbootaop.services.GreetingService.*(..))") 
+    @Around("greetingLoggerPointCut()")
     public Object loggerAround(ProceedingJoinPoint joinPoint) throws Throwable{ //ProceedingJoinPoint se esta ejecutando este aspecto va a envolver la ejecucion
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
--- new-spring-boot-webapp/springboot-aop/springboot-aop/src/main/java/com/gerson/springboot/app/aop/springbootaop/aop/GreetingFooAspect.java
+++ new-spring-boot-webapp/springboot-aop/springboot-aop/src/main/java/com/gerson/springboot/app/aop/springbootaop/aop/GreetingFooAspect.java
@@ -6,6 +6,7 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
@@ -18,7 +19,12 @@ public class GreetingFooAspect {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Before("execution(* com.gerson.springboot.app.aop.springbootaop.services.GreetingService.*(..))") // Agregar * en el tipo de dato a retornar "seria cualquier". //Punto de corte cual metodo o package vamos a interceptar. Execution recive("String: tipo de dato que devuelve el metodo, la ruta completa, nombre de la clase y el metodo y sus argumentos") al agregar el GreetingService.*(..) todas las clases que implementen esta interfaz
+     //Metodo para definir el punto de corte para evitar repetir en cada metodo
+    @Pointcut("execution(* com.gerson.springboot.app.aop.springbootaop.services.GreetingService.*(..))") //punto de corte
+    private void greetingFoodLoggerPointCut(){}
+
+    //@Before("execution(* com.gerson.springboot.app.aop.springbootaop.services.GreetingService.*(..))") // Agregar * en el tipo de dato a retornar "seria cualquier". //Punto de corte cual metodo o package vamos a interceptar. Execution recive("String: tipo de dato que devuelve el metodo, la ruta completa, nombre de la clase y el metodo y sus argumentos") al agregar el GreetingService.*(..) todas las clases que implementen esta interfaz
+    @Before("greetingFoodLoggerPointCut()")
     public void loggerBefore(JoinPoint joinPoint){ //Este metodo se va a a ejecutar antes del metodo especificado en @Before
         String method = joinPoint.getSignature().getName(); //Obtenemos el nombre del metodo
         String args = Arrays .toString(joinPoint.getArgs());//Obtenemos los argumentos del metodo
@@ -27,7 +33,8 @@ public class GreetingFooAspect {
     }
 
         //After - Especifica que el metodo loggerAfter se ejecutara despues que el metodo especificado en execution
-    @After("execution(* com.gerson.springboot.app.aop.springbootaop.services.GreetingService.*(..))") 
+    //@After("execution(* com.gerson.springboot.app.aop.springbootaop.services.GreetingService.*(..))") 
+    @After("greetingFoodLoggerPointCut()")
     public void loggerAfter(JoinPoint joinPoint){ //Este metodo se va a a ejecutar antes del metodo especificado en @After
         String method = joinPoint.getSignature().getName(); //Obtenemos el nombre del metodo
         String args = Arrays .toString(joinPoint.getArgs());//Obtenemos los argumentos del metodo
